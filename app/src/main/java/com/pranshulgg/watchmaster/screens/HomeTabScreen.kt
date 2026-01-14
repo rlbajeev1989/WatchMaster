d2f8cfc6481df13f39bbc8ba8d7ce6b945c0fa18
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-14 20:14:50 +0530

    Update settings screen logic and add TMDB API integration

---
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
@@ -0,0 +1,29 @@
+package com.pranshulgg.watchmaster.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.pranshulgg.watchmaster.prefs.LocalAppPrefs
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTabScreen(
+) {
+    val prefs = LocalAppPrefs.current
+
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+    ) { innerPadding ->
+
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text("cool")
+
+        }
+
+    }
+}
