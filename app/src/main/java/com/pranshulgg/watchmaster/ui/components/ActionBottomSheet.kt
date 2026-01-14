ad6a56f3d064458a09ac056282b542bd3d0155b2
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-14 11:51:27 +0530

    Add ActionBottomSheet component

---
+++ app/src/main/java/com/pranshulgg/watchmaster/ui/components/ActionBottomSheet.kt
@@ -0,0 +1,87 @@
+package com.pranshulgg.watchmaster.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun ActionBottomSheet(
+    sheetState: SheetState,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+    confirmText: String = "Save",
+    cancelText: String = "Cancel",
+    content: @Composable ColumnScope.() -> Unit
+) {
+    if (sheetState.isVisible) {
+        ModalBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = onCancel,
+            containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+
+            ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = 500.dp)
+            ) {
+
+                content()
+
+                Spacer(Modifier.height(12.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 16.dp, start = 16.dp, bottom = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(
+                        modifier = Modifier.defaultMinSize(minWidth = 90.dp, minHeight = 45.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.errorContainer),
+                        onClick = {
+                            onCancel()
+                        }, shapes = ButtonDefaults.shapes()
+                    ) {
+                        Text(
+                            cancelText,
+                            color = MaterialTheme.colorScheme.onErrorContainer,
+                            fontSize = 16.sp
+                        )
+                    }
+                    Button(
+                        onClick = {
+                            onConfirm()
+                        },
+
+                        shapes = ButtonDefaults.shapes(),
+                        modifier = Modifier.defaultMinSize(minWidth = 90.dp, minHeight = 45.dp),
+                    ) {
+                        Text(confirmText, fontSize = 16.sp)
+                    }
+
+                }
+            }
+        }
+    }
+}
