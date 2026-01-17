f103cd0b0b788b3045e149a57bfd55d4d3b32119
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-17 15:35:16 +0530

    Update SearchScreen with Material 3 components and BottomSheetScaffold

--- app/src/main/java/com/pranshulgg/watchmaster/screens/search/SearchScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/search/SearchScreen.kt
@@ -1,104 +1,192 @@
 package com.pranshulgg.watchmaster.screens.search
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeFlexibleTopAppBar
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import com.pranshulgg.watchmaster.R
+import com.pranshulgg.watchmaster.ui.components.EmptyContainerPlaceholder
+import com.pranshulgg.watchmaster.utils.NavigateUpBtn
+import com.pranshulgg.watchmaster.utils.Symbol
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.room.Index
+import com.pranshulgg.watchmaster.data.getGenreNames
+import com.pranshulgg.watchmaster.screens.search.ui.SearchBottomSheetContent
+import com.pranshulgg.watchmaster.screens.search.ui.SearchRow
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun SearchScreen(
-    viewModel: SearchViewModel = viewModel(factory = SearchViewModelFactory())
+    viewModel: SearchViewModel = viewModel(factory = SearchViewModelFactory(LocalContext.current)),
+    navController: NavController
 ) {
     val query = viewModel.query
     val results = viewModel.results
     val loading = viewModel.loading
 
-    Column(modifier = Modifier.fillMaxSize()) {
 
-        OutlinedTextField(
-            value = query,
-            onValueChange = viewModel::onQueryChange,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(12.dp),
-            placeholder = { Text("Search TMDB") },
-            singleLine = true
-        )
+    val scope = rememberCoroutineScope()
 
-        Button(
-            onClick = { viewModel.search() },
-            modifier = Modifier
-                .padding(horizontal = 12.dp)
-                .fillMaxWidth()
-        ) {
-            Text("Search")
-        }
+    val searchBtnSize = ButtonDefaults.MediumContainerHeight
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val listState = rememberLazyListState()
 
-        if (loading) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator()
-            }
-        }
 
-        LazyColumn {
-            items(results) { item ->
-                SearchRow(item)
-            }
-        }
-    }
-}
 
-@Composable
-fun SearchRow(item: SearchItem) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(12.dp)
-    ) {
-        val poster = item.posterPath?.let {
-            "https://image.tmdb.org/t/p/w154$it"
-        }
-
-        AsyncImage(
-            model = poster,
-            contentDescription = item.title,
-            modifier = Modifier.size(64.dp)
-        )
-
-        Spacer(Modifier.width(12.dp))
-
-        Column {
-            Text(item.title, style = MaterialTheme.typography.titleMedium)
-            Text(
-                item.mediaType.uppercase(),
-                style = MaterialTheme.typography.bodySmall
+    BottomSheetScaffold(
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        topBar = {
+            LargeFlexibleTopAppBar(
+                title = { Text(text = "Search") },
+                navigationIcon = { NavigateUpBtn(navController) },
+                scrollBehavior = scrollBehavior,
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    ),
             )
-            item.overview?.let {
-                Text(it, maxLines = 2, style = MaterialTheme.typography.bodySmall)
+        },
+        sheetContent = {
+            SearchBottomSheetContent(viewModel, query, focusRequester, focusManager)
+        },
+        sheetPeekHeight = 100.dp,
+        sheetDragHandle = null,
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            when {
+                loading -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        LoadingIndicator()
+                    }
+                }
+
+                results.isEmpty() -> {
+                    EmptyContainerPlaceholder(
+                        R.drawable.search_24px,
+                        "Search movies",
+                        description = "Search through the movie database"
+                    )
+                }
+
+                else -> {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = 10.dp)
+                    ) {
+                        itemsIndexed(results) { index, item ->
+                            SearchRow(item, index, results, onClick = {
+                                viewModel.addToWatchlist(item)
+                            })
+                        }
+
+                    }
+                }
             }
         }
     }
+
 }
+
+
+
