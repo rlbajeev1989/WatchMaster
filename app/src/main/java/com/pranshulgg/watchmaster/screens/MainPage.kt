2d4c52844caac6e6c362cece57af314dd34b4de3
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-21 21:16:21 +0530

    Refactor navigation and update MovieTabHomeScreen with scroll behaviors

--- app/src/main/java/com/pranshulgg/watchmaster/screens/MainPage.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/MainPage.kt
@@ -4,23 +4,30 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarDefaults
+import androidx.compose.material3.FloatingToolbarExitDirection.Companion.Bottom
+import androidx.compose.material3.HorizontalFloatingToolbar
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
+import com.pranshulgg.watchmaster.helpers.NavRoutes
 import com.pranshulgg.watchmaster.models.HomeNavViewModel
 import com.pranshulgg.watchmaster.ui.components.BottomNav
 import com.pranshulgg.watchmaster.ui.components.Tooltip
@@ -34,17 +41,22 @@ import com.pranshulgg.watchmaster.utils.Symbol
 fun MainScreen(
     navController: NavController,
     viewModel: HomeNavViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
-    motionScheme: MotionScheme
+    motionScheme: MotionScheme,
 ) {
 
     val selectedItem = viewModel.selectedItem
     val appBarTitles = listOf("Home", "Movies", "TV series")
     val context = LocalContext.current
 
+    val scrollBehavior =
+        FloatingToolbarDefaults.exitAlwaysScrollBehavior(exitDirection = Bottom)
 
+    val scrollBehaviorTopBar = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
         topBar = {
             TopAppBar(
+                scrollBehavior = scrollBehaviorTopBar,
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.surfaceContainer
                 ),
@@ -58,7 +70,7 @@ fun MainScreen(
                         spacing = 10.dp
                     ) {
                         IconButton(
-                            onClick = { navController.navigate("settingsPage") },
+                            onClick = { navController.navigate(NavRoutes.SETTINGS) },
                             shapes = IconButtonDefaults.shapes()
                         ) {
                             Symbol(
@@ -74,16 +86,24 @@ fun MainScreen(
         bottomBar = {
             BottomNav(
                 selectedItem = selectedItem,
-                onItemSelected = { index -> viewModel.selectedItem = index }
+                onItemSelected = { index -> viewModel.selectedItem = index },
+                navController,
+                scrollBehavior
             )
         }
     ) { innerPadding ->
         Box(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier.padding(top = innerPadding.calculateTopPadding())
         ) {
+
             when (selectedItem) {
                 0 -> HomeTabScreen()
-                1 -> MovieTabScreen(navController, motionScheme)
+                1 -> MovieTabHomeScreen(
+                    navController,
+                    motionScheme,
+                    scrollBehavior,
+                    scrollBehaviorTopBar
+                )
             }
 
         }
---
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/MovieTabHomeScreen.kt
@@ -0,0 +1,113 @@
+package com.pranshulgg.watchmaster.screens
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pranshulgg.watchmaster.utils.Symbol
+import kotlinx.coroutines.launch
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.pranshulgg.watchmaster.R
+import com.pranshulgg.watchmaster.data.local.WatchMasterDatabase
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.model.WatchStatus
+import com.pranshulgg.watchmaster.data.repository.WatchlistRepository
+import com.pranshulgg.watchmaster.models.WatchlistViewModel
+import com.pranshulgg.watchmaster.models.WatchlistViewModelFactory
+import com.pranshulgg.watchmaster.screens.movieTabs.finished.FinishedMovies
+import com.pranshulgg.watchmaster.screens.movieTabs.watching.WatchingMovies
+import com.pranshulgg.watchmaster.screens.movieTabs.watchlist.WatchlistMovies
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun MovieTabHomeScreen(
+    navController: NavController,
+    motionScheme: MotionScheme,
+    scrollBehavior: FloatingToolbarScrollBehavior,
+    scrollBehaviorTopBar: TopAppBarScrollBehavior
+) {
+
+    val tabs = listOf("Watchlist", "Watching", "Finished")
+    val pagerState = rememberPagerState { tabs.size }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    val db = WatchMasterDatabase.getInstance(context)
+    val repository = WatchlistRepository(db.watchlistDao())
+
+    val viewModel: WatchlistViewModel = viewModel(
+        factory = WatchlistViewModelFactory(repository)
+    )
+
+    val items by viewModel.watchlist.collectAsState()
+
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        topBar = {
+            PrimaryTabRow(
+                containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                selectedTabIndex = pagerState.currentPage
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+
+                        },
+                        text = { Text(title) }
+                    )
+                }
+            }
+        },
+        contentWindowInsets = WindowInsets(bottom = 0, top = 0),
+    ) { innerPadding ->
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) { page ->
+            when (page) {
+                0 -> WatchlistMovies(
+                    items = items.filter {
+                        it.status == WatchStatus.WANT_TO_WATCH && it.mediaType != "tv"
+                    },
+                    scrollBehavior,
+                    scrollBehaviorTopBar
+                )
+
+                1 -> WatchingMovies(
+                    items = items.filter {
+                        it.status != WatchStatus.WATCHING && it.mediaType != "tv"
+                    }
+                )
+
+                2 -> FinishedMovies(
+                    items = items.filter {
+                        it.status == WatchStatus.FINISHED && it.mediaType != "tv"
+                    }
+                )
+            }
+        }
+    }
+}
+
+
