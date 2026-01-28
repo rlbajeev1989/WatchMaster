eba232f8910a1aac3d3b7f8b6cfcb93e80f54d50
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-28 10:15:00 +0530

    Implement LazyColumn and nested scrolling in WatchingMovies screen

--- app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watching/WatchingScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watching/WatchingScreen.kt
@@ -1,14 +1,43 @@
 package com.pranshulgg.watchmaster.screens.movieTabs.watching
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarScrollBehavior
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.screens.movieTabs.ui.WatchlistRow
 
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun WatchingMovies(items: List<WatchlistItemEntity>) {
-    Text("Currently watching 🎬", modifier = Modifier.padding(16.dp))
+fun WatchingMovies(
+    items: List<WatchlistItemEntity>, scrollBehavior: FloatingToolbarScrollBehavior,
+    scrollBehaviorTopBar: TopAppBarScrollBehavior,
+    navController: NavController
+
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior)
+            .nestedScroll(scrollBehaviorTopBar.nestedScrollConnection)
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp)
+    ) {
+        itemsIndexed(items) { index, item ->
+            WatchlistRow(item, index, items, navController)
+        }
+
+    }
 }
