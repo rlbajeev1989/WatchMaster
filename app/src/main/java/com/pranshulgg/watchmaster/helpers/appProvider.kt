07f6cc7f1cba3a202b0db54fcf82eb6ebc0b60c3
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-28 09:57:39 +0530

    Update appProvider to include MovieRepository in WatchlistRepository initialization

--- app/src/main/java/com/pranshulgg/watchmaster/helpers/appProvider.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/helpers/appProvider.kt
@@ -3,9 +3,20 @@ package com.pranshulgg.watchmaster.helpers
 import android.content.Context
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pranshulgg.watchmaster.data.local.WatchMasterDatabase
+import com.pranshulgg.watchmaster.data.repository.MovieRepository
 import com.pranshulgg.watchmaster.data.repository.WatchlistRepository
+import com.pranshulgg.watchmaster.network.TmdbApi
 
 fun provideWatchlistRepository(context: Context): WatchlistRepository {
     val db = WatchMasterDatabase.getInstance(context.applicationContext)
-    return WatchlistRepository(db.watchlistDao())
+
+    val movieRepo = MovieRepository(
+        api = TmdbApi.create(),
+        dao = db.movieBundleDao()
+    )
+
+    return WatchlistRepository(
+        dao = db.watchlistDao(),
+        movieRepository = movieRepo
+    )
 }
