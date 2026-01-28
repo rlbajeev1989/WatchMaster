40a336fbaa4b06e45c0e08e116c5ff51879badeb
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-01-28 10:11:06 +0530

    Add media detail route to navigation system

--- app/src/main/java/com/pranshulgg/watchmaster/helpers/NavRoutes.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/helpers/NavRoutes.kt
@@ -7,6 +7,12 @@ object NavRoutes {
     const val SETTINGS = "settings"
     const val SEARCH = "search"
 
+    const val MEDIA_DETAIL_PAGE = "mediadetail"
+
+    fun mediaDetail(id: Long): String {
+        return "$MEDIA_DETAIL_PAGE/$id"
+    }
+
     fun search(type: SearchType = SearchType.MULTI): String {
         return "$SEARCH?type=${type.name}"
     }
