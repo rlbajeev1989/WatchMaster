aaf1a3ac4700300a5760a8c1edf2288651f08802
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-03 20:00:36 +0530

    Update start destination to media detail in MainActivity

--- app/src/main/java/com/pranshulgg/watchmaster/MainActivity.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/MainActivity.kt
@@ -86,8 +86,8 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier
                                 .background(MaterialTheme.colorScheme.surfaceContainer),
                             navController = navController,
-//                            startDestination = NavRoutes.mediaDetail(550),
-                            startDestination = NavRoutes.MAIN,
+                            startDestination = NavRoutes.mediaDetail(550),
+//                            startDestination = NavRoutes.MAIN,
                             enterTransition = {
                                 NavTransitions.enter(motionScheme)
                             },
