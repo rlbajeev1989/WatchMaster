9e15ebcb52d9594b7f56c497d1feda1c0333ae62
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-07 21:17:25 +0530

    Update HomeTabScreen UI placeholder

--- app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
@@ -1,12 +1,15 @@
 package com.pranshulgg.watchmaster.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.pranshulgg.watchmaster.prefs.LocalAppPrefs
 
@@ -21,7 +24,9 @@ fun HomeTabScreen(
     ) { innerPadding ->
 
         Column(modifier = Modifier.padding(innerPadding)) {
-            Text("cool")
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("Nothing here yet")
+            }
 
         }
 
