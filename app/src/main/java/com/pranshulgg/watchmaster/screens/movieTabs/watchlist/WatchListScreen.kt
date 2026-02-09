73dd0545f9bc8680d36a08e8002260bb11a52fbf
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-09 09:26:02 +0530

    Add empty state placeholder to WatchListScreen

--- app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watchlist/WatchListScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watchlist/WatchListScreen.kt
@@ -1,6 +1,7 @@
 package com.pranshulgg.watchmaster.screens.movieTabs.watchlist
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,14 +9,19 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingToolbarScrollBehavior
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.pranshulgg.watchmaster.R
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.model.SearchType
 import com.pranshulgg.watchmaster.screens.movieTabs.ui.WatchlistRow
+import com.pranshulgg.watchmaster.ui.components.EmptyContainerPlaceholder
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -23,9 +29,17 @@ fun WatchlistMovies(
     items: List<WatchlistItemEntity>,
     scrollBehavior: FloatingToolbarScrollBehavior,
     scrollBehaviorTopBar: TopAppBarScrollBehavior,
-    navController: NavController
+    navController: NavController,
 ) {
 
+
+    if (items.isEmpty()) {
+        EmptyContainerPlaceholder(
+            R.drawable.movie_info_24px,
+            "No movies found",
+            description = "Your watchlist is empty. Add some movies to start building it!"
+        )
+    }
 
     LazyColumn(
         modifier = Modifier
@@ -36,6 +50,7 @@ fun WatchlistMovies(
         verticalArrangement = Arrangement.spacedBy(2.dp)
     ) {
         itemsIndexed(items) { index, item ->
+
             WatchlistRow(item, index, items, navController)
         }
 
