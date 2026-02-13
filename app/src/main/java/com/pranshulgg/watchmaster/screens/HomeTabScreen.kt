4138c5bab7fa27094fba5f8b05434cfaff971879
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-13 21:12:14 +0530

    Update HomeTabScreen imports and structure

--- app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.pranshulgg.watchmaster.R
 import com.pranshulgg.watchmaster.prefs.LocalAppPrefs
+import com.pranshulgg.watchmaster.utils.Symbol
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,6 +30,7 @@ fun HomeTabScreen(
         Column(modifier = Modifier.padding(innerPadding)) {
             Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 Text("Nothing here yet")
+
             }
 
         }
