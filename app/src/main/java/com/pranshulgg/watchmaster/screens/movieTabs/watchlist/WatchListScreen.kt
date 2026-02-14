ca60cf1fb92bb682933f8f37cf79e536e14583b4
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-14 22:03:07 +0530

    Update WatchListScreen to handle long-press actions

--- app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watchlist/WatchListScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watchlist/WatchListScreen.kt
@@ -12,16 +12,25 @@ import androidx.compose.material3.FloatingToolbarScrollBehavior
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
 import com.pranshulgg.watchmaster.model.SearchType
+import com.pranshulgg.watchmaster.models.WatchlistViewModel
 import com.pranshulgg.watchmaster.screens.movieTabs.ui.WatchlistRow
 import com.pranshulgg.watchmaster.ui.components.EmptyContainerPlaceholder
+import com.pranshulgg.watchmaster.ui.components.TextAlertDialog
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -30,7 +39,10 @@ fun WatchlistMovies(
     scrollBehavior: FloatingToolbarScrollBehavior,
     scrollBehaviorTopBar: TopAppBarScrollBehavior,
     navController: NavController,
+    onLongActionMovieRequest: (WatchlistItemEntity) -> Unit
 ) {
+
+    val watchlistViewModel: WatchlistViewModel = viewModel()
 
 
     if (items.isEmpty()) {
@@ -51,8 +63,15 @@ fun WatchlistMovies(
     ) {
         itemsIndexed(items) { index, item ->
 
-            WatchlistRow(item, index, items, navController)
+            WatchlistRow(
+                item,
+                index,
+                items,
+                navController,
+                onLongActionMovieRequest = { onLongActionMovieRequest(item) })
         }
 
     }
+
+
 }
