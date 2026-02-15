52094d34ae12a5563d9c8a4b9996e96024494d52
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-15 19:27:21 +0530

    Update watchlist confirmation action in SearchScreen

--- app/src/main/java/com/pranshulgg/watchmaster/screens/media_detail/ui/SectionCard.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/media_detail/ui/SectionCard.kt
@@ -1,16 +1,23 @@
 package com.pranshulgg.watchmaster.screens.media_detail.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,9 +30,16 @@ import com.pranshulgg.watchmaster.R
 import com.pranshulgg.watchmaster.utils.Radius
 import com.pranshulgg.watchmaster.utils.Symbol
 
-@OptIn(ExperimentalTextApi::class)
+@OptIn(ExperimentalTextApi::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun SectionCard(title: String, titleIcon: Int, content: @Composable () -> Unit) {
+fun SectionCard(
+    title: String,
+    titleIcon: Int,
+    showAction: Boolean = false,
+    actionText: String = "",
+    actionOnClick: () -> Unit = {},
+    content: @Composable () -> Unit,
+) {
 
     Card(
         modifier = Modifier
@@ -49,7 +63,6 @@ fun SectionCard(title: String, titleIcon: Int, content: @Composable () -> Unit) 
                     titleIcon,
                     color = MaterialTheme.colorScheme.secondary
                 )
-
                 Text(
                     title,
                     style = MaterialTheme.typography.titleMedium,
@@ -64,6 +77,13 @@ fun SectionCard(title: String, titleIcon: Int, content: @Composable () -> Unit) 
                     ),
                     color = MaterialTheme.colorScheme.secondary
                 )
+
+                if (showAction) {
+                    Spacer(Modifier.weight(1f))
+                    HeaderAction(text = actionText, onClick = { actionOnClick() })
+                }
+
+
             }
             content()
         }
@@ -71,3 +91,23 @@ fun SectionCard(title: String, titleIcon: Int, content: @Composable () -> Unit) 
 
 }
 
+
+@Composable
+private fun HeaderAction(text: String, onClick: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable(
+                onClick = {
+                    onClick()
+                }
+            )
+            .padding(end = 5.dp)
+    ) {
+        Text(
+            text,
+            style = MaterialTheme.typography.labelLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
