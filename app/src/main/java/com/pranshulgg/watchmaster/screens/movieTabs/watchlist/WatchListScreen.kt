7580e72691adbf01e0331fc278551b37081fcd8f
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-16 22:09:27 +0530

    Refactor WatchListScreen to use MovieItems and implement pinned item filtering

--- app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watchlist/WatchListScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/movieTabs/watchlist/WatchListScreen.kt
@@ -1,14 +1,26 @@
 package com.pranshulgg.watchmaster.screens.movieTabs.watchlist
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarDefaults.ScreenOffset
 import androidx.compose.material3.FloatingToolbarScrollBehavior
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
@@ -20,14 +32,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
 import com.pranshulgg.watchmaster.model.SearchType
 import com.pranshulgg.watchmaster.models.WatchlistViewModel
+import com.pranshulgg.watchmaster.screens.movieTabs.ui.MovieItems
 import com.pranshulgg.watchmaster.screens.movieTabs.ui.WatchlistRow
 import com.pranshulgg.watchmaster.ui.components.EmptyContainerPlaceholder
 import com.pranshulgg.watchmaster.ui.components.TextAlertDialog
@@ -53,25 +68,17 @@ fun WatchlistMovies(
         )
     }
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .nestedScroll(scrollBehavior)
-            .nestedScroll(scrollBehaviorTopBar.nestedScrollConnection)
-            .padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(2.dp)
-    ) {
-        itemsIndexed(items) { index, item ->
 
-            WatchlistRow(
-                item,
-                index,
-                items,
-                navController,
-                onLongActionMovieRequest = { onLongActionMovieRequest(item) })
-        }
-
-    }
+    val pinnedItems = items.filter { it.isPinned }
+    val normalItems = items.filter { !it.isPinned }
 
 
+    MovieItems(
+        scrollBehavior,
+        scrollBehaviorTopBar,
+        navController,
+        onLongActionMovieRequest,
+        pinnedItems,
+        normalItems
+    )
 }
