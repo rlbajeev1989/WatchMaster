bc44f29c523ce5613777be64a30781e9b9a68e27
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-19 17:18:08 +0530

    feat: Add temporary navigation button to HomeTabScreen

--- app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/HomeTabScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,13 +14,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
+import com.pranshulgg.watchmaster.helpers.NavRoutes
 import com.pranshulgg.watchmaster.prefs.LocalAppPrefs
 import com.pranshulgg.watchmaster.utils.Symbol
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeTabScreen(
+    navController: NavController
 ) {
     val prefs = LocalAppPrefs.current
 
@@ -31,6 +35,11 @@ fun HomeTabScreen(
             Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 Text("Nothing here yet")
 
+                Button(onClick = {
+                    navController.navigate(NavRoutes.mediaDetail(243875, false))
+                }) {
+                    Text("GO TO IDK TV")
+                }
             }
 
         }
