28868929e3f5ddb1af8e8c1990e1e37884a72e57
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-22 21:04:28 +0530

    Update SearchViewModel mock data and enable initial state

--- app/src/main/java/com/pranshulgg/watchmaster/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/screens/search/SearchViewModel.kt
@@ -39,14 +39,14 @@ class SearchViewModel(
 
     private val mockResults = listOf(
         SearchItem(
-            id = 2640,
-            title = "Crayon Shin-chan: Fast Asleep! Dreaming World Big Assault!",
-            posterPath = "/xgHTpx8dywe3QTd5iuKuB0g3SNv.jpg",
+            id = 243875,
+            title = "Georgie & Mandy's First Marriage",
+            posterPath = "/3z2mYFxUkzanb2eeIcVyfJq0G3q.jpg",
             mediaType = "tv",
-            releaseDate = "1978-05-17",
-            genreIds = listOf(28, 878, 28, 878),
-            overview = "One night, the Nohara family were enjoying a pleasant dream, when suddenly a big fish appeared in their dreams and ate them. The next morning, Hiroshi read in the newspaper that everybody in another town had the same nightmare as him, but it seemed to have ended. But Hiroshi also heard the same dream from Misae, Shinnosuke, Himawari and even Shiro. They were surprised and thought if the same thing is happening in Kasukabe too. In kindergarten, on telling others about his nightmare, Shinnosuke was surprised to know that everybody too had the same dream. Then a mysterious girl named Saki was transferred to Futaba Kindergarten and joined Shinnosuke's class. Everyone in the class, including the rather inactive Bo-chan, were all excited and happy on seeing her. But Saki had a cold attitude and didn't get along well.",
-            backdropPath = ""
+            releaseDate = "2024-10-17",
+            genreIds = listOf(35),
+            overview = "Georgie and Mandy raise their young family in Texas while navigating the challenges of adulthood, parenting, and marriage.",
+            backdropPath = "/pCr3YYJkkLt9fdGVD7I2Z1l6lzK.jpg"
 
         ),
         SearchItem(
@@ -97,10 +97,10 @@ class SearchViewModel(
     )
 
 
-//    init {
-//        results = mockResults
-//        loading = false
-//    }
+    init {
+        results = mockResults
+        loading = false
+    }
 
     fun search(type: SearchType = SearchType.MULTI) {
         if (query.isBlank()) return
