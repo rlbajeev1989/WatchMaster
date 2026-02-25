7f2f87f1200e268c883a469f3bc37c47544734e3
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 20:50:53 +0530

    Rename HomeTabScreen to HomeScreen and move to feature/home package

--- app/src/main/java/com/pranshulgg/watchmaster/feature/home/HomeScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/home/HomeScreen.kt
@@ -1,28 +1,22 @@
-package com.pranshulgg.watchmaster.screens
+package com.pranshulgg.watchmaster.feature.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import com.pranshulgg.watchmaster.R
-import com.pranshulgg.watchmaster.helpers.NavRoutes
-import com.pranshulgg.watchmaster.prefs.LocalAppPrefs
-import com.pranshulgg.watchmaster.utils.Symbol
+import com.pranshulgg.watchmaster.core.prefs.LocalAppPrefs
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeTabScreen(
+fun HomeScreen(
     navController: NavController
 ) {
     val prefs = LocalAppPrefs.current
