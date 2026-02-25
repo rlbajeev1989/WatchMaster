5ff746f309542189eab627674fcfcba9124b9aca
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:12:50 +0530

    Update SeasonDataMapper to use SeasonData model

--- app/src/main/java/com/pranshulgg/watchmaster/data/local/mapper/SeasonDataMapper.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/data/local/mapper/SeasonDataMapper.kt
@@ -2,19 +2,19 @@ package com.pranshulgg.watchmaster.data.local.mapper
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.pranshulgg.watchmaster.model.SeasonDataModel
+import com.pranshulgg.watchmaster.core.model.SeasonData
 
 object SeasonDataMapper {
 
     private val gson = Gson()
-    private val type = object : TypeToken<List<SeasonDataModel>>() {}.type
+    private val type = object : TypeToken<List<SeasonData>>() {}.type
 
-    fun toJson(seasons: List<SeasonDataModel>?): String? {
+    fun toJson(seasons: List<SeasonData>?): String? {
         if (seasons.isNullOrEmpty()) return null
         return gson.toJson(seasons)
     }
 
-    fun fromJson(json: String?): List<SeasonDataModel> {
+    fun fromJson(json: String?): List<SeasonData> {
         if (json.isNullOrBlank()) return emptyList()
 
         return try {
