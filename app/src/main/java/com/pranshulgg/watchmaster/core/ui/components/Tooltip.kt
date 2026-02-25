78aca8436e5d3ee014399728f788a4fceaa0ff1f
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:17:35 +0530

    Move Tooltip component to core module

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/Tooltip.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/Tooltip.kt
@@ -1,8 +1,7 @@
-package com.pranshulgg.watchmaster.ui.components
+package com.pranshulgg.watchmaster.core.ui.components
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
