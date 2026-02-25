865fd532eac163191bc21f47b5c0f40ecdd879bf
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:20:43 +0530

    Refactor: Move TvDetailsViewModel to feature package

--- app/src/main/java/com/pranshulgg/watchmaster/feature/tv/detail/TvDetailsViewModel.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/detail/TvDetailsViewModel.kt
@@ -1,13 +1,11 @@
-package com.pranshulgg.watchmaster.models
+package com.pranshulgg.watchmaster.feature.tv.detail
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.pranshulgg.watchmaster.data.local.entity.MovieBundle
 import com.pranshulgg.watchmaster.data.local.entity.TvBundle
-import com.pranshulgg.watchmaster.data.repository.MovieRepository
 import com.pranshulgg.watchmaster.data.repository.TvRepository
 import kotlinx.coroutines.launch
 
