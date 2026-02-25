9ba5d5af914482f9e1e9742305974afe1ff049c4
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:05:23 +0530

    Move NavigateUpButton to core.ui.components package

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/NavigateUpButton.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/NavigateUpButton.kt
@@ -1,19 +1,15 @@
-package com.pranshulgg.watchmaster.utils
+package com.pranshulgg.watchmaster.core.ui.components
 
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
-import com.pranshulgg.watchmaster.ui.components.Tooltip
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
