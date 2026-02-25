215ad0bbc9902c9aefa4e1a14f5ffdfaff243ab0
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:07:32 +0530

    Move PosterPlaceholder to core.ui.components.media package

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/media/PosterPlaceholder.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/media/PosterPlaceholder.kt
@@ -1,13 +1,10 @@
-package com.pranshulgg.watchmaster.ui.components
+package com.pranshulgg.watchmaster.core.ui.components.media
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -15,22 +12,13 @@ import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import coil.size.Size
-import com.pranshulgg.watchmaster.R
-import com.pranshulgg.watchmaster.utils.Symbol
 
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
