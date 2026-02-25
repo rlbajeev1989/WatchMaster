97fa698b795aaa859474e1cfdd4678f7709cbf97
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 20:51:16 +0530

    Move LargeTopBarScaffold to core package

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/LargeTopBarScaffold.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/LargeTopBarScaffold.kt
@@ -1,4 +1,4 @@
-package com.pranshulgg.watchmaster.ui.components
+package com.pranshulgg.watchmaster.core.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
