1988eb535a3d39b26f2be80eca242eda960505e0
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:08:19 +0530

    Move ProfilePlaceholder to core ui components media package

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/media/ProfilePlaceholder.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/media/ProfilePlaceholder.kt
@@ -1,4 +1,4 @@
-package com.pranshulgg.watchmaster.ui.components
+package com.pranshulgg.watchmaster.core.ui.components.media
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
