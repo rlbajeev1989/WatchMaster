2c6889039e059d08de72d1f2168205a05a7e0c3e
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 21:02:40 +0530

    Move MovieItems.kt to movie feature components package

--- app/src/main/java/com/pranshulgg/watchmaster/feature/movie/components/MovieItems.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/components/MovieItems.kt
@@ -1,4 +1,4 @@
-package com.pranshulgg.watchmaster.screens.movieTabs.ui
+package com.pranshulgg.watchmaster.feature.movie.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
