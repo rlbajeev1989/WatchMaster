52b6d23adbba5dc6056cd2760e9318a87fa1a416
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-25 20:49:28 +0530

    Move DialogSliderTile to core package and clean up imports

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/tiles/DialogSliderTile.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/tiles/DialogSliderTile.kt
@@ -1,41 +1,21 @@
-package com.pranshulgg.watchmaster.ui.components.tiles
+package com.pranshulgg.watchmaster.core.ui.components.tiles
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsDraggedAsState
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme.motionScheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
-import com.pranshulgg.watchmaster.utils.Symbol
-import java.time.temporal.TemporalQueries.offset
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
