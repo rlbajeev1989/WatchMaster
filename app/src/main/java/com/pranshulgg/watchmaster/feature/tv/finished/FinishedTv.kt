99ee30fcbc7e0948416262be8ee1ff741c4b05b1
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-28 12:27:33 +0530

    Add FinishedTv screen and its loading state

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/finished/FinishedTv.kt
@@ -0,0 +1,11 @@
+package com.pranshulgg.watchmaster.feature.tv.finished
+
+import androidx.compose.runtime.Composable
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+
+@Composable
+fun FinishedTv(
+    isLoading: Boolean,
+    items: List<WatchlistItemEntity>
+) {
+}
