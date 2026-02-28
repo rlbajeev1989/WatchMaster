c70d231a0ac6870f1e0083c6acf505559a553163
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-28 12:31:12 +0530

    Add WatchingTv composable screen

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/watching/WatchingTv.kt
@@ -0,0 +1,12 @@
+package com.pranshulgg.watchmaster.feature.tv.watching
+
+import androidx.compose.runtime.Composable
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+
+
+@Composable
+fun WatchingTv(
+    isLoading: Boolean,
+    items: List<WatchlistItemEntity>
+) {
+}
