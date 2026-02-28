5b27b7549eb381ffc5bc6c8ec15f710d0ceb1170
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-02-28 12:30:54 +0530

    Update TvTabsContent to display watchlist, watching, and finished TV screens

--- app/src/main/java/com/pranshulgg/watchmaster/feature/tv/TvTabsContent.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/TvTabsContent.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarScrollBehavior
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -19,10 +23,18 @@ import com.pranshulgg.watchmaster.feature.movie.MovieTab
 import com.pranshulgg.watchmaster.feature.movie.finished.FinishedMovies
 import com.pranshulgg.watchmaster.feature.movie.watching.WatchingMovies
 import com.pranshulgg.watchmaster.feature.movie.watchlist.WatchlistMovies
+import com.pranshulgg.watchmaster.feature.tv.finished.FinishedTv
+import com.pranshulgg.watchmaster.feature.tv.watching.WatchingTv
+import com.pranshulgg.watchmaster.feature.tv.watchlist.WatchlistTv
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun TvTabsContent(navController: NavController) {
+fun TvTabsContent(
+    navController: NavController, state: TvHomeState,
+    scrollBehavior: FloatingToolbarScrollBehavior,
+    scrollBehaviorTopBar: TopAppBarScrollBehavior,
+) {
 
     val tabs = TvTab.entries
     val pagerState = rememberPagerState { tabs.size }
@@ -59,7 +71,29 @@ fun TvTabsContent(navController: NavController) {
             verticalAlignment = Alignment.Top
         ) { page ->
 
-            Text("${tabs[page]}")
+            when (tabs[page]) {
+
+                TvTab.WATCHLIST -> {
+                    WatchlistTv(
+                        state.isLoading,
+                        state.watchlist,
+                        scrollBehavior,
+                        scrollBehaviorTopBar,
+                        navController,
+                        onLongActionTvRequest = {},
+                    )
+                }
+
+                TvTab.WATCHING -> {
+                    WatchingTv(state.isLoading, state.watching)
+                }
+
+                TvTab.FINISHED -> {
+                    FinishedTv(state.isLoading, state.finished)
+                }
+
+            }
+
 
         }
     }
