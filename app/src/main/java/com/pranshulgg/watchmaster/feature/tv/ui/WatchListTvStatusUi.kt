68d43e355fb1240ddc2483b245d721de30cb974e
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-01 20:25:39 +0530

    Add WatchListTvStatusUi data class and status mapping function

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/ui/WatchListTvStatusUi.kt
@@ -0,0 +1,58 @@
+package com.pranshulgg.watchmaster.feature.tv.ui
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
+import com.pranshulgg.watchmaster.core.model.WatchStatus
+import com.pranshulgg.watchmaster.core.ui.theme.LocalStatusColors
+import com.pranshulgg.watchmaster.core.utils.formatDate
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistSeasonEntity
+
+
+data class WatchListTvStatusUi(
+    val statusLabel: String,
+    val containerColor: Color,
+    val contentColor: Color
+)
+
+@Composable
+fun WatchStatus.toWatchListTvStatusUi(item: WatchlistSeasonEntity): WatchListTvStatusUi {
+    val statusColor = LocalStatusColors.current
+
+    return when (this) {
+        WatchStatus.WATCHING -> {
+            WatchListTvStatusUi(
+                statusLabel = "Started • ${item.seasonStartedDate?.formatDate()}",
+                containerColor = statusColor.warning.bg,
+                contentColor = statusColor.warning.on
+            )
+        }
+
+        WatchStatus.FINISHED -> {
+            WatchListTvStatusUi(
+                statusLabel = "Finished • ${item.seasonFinishedDate?.formatDate()}",
+                containerColor = statusColor.success.bg,
+                contentColor = statusColor.success.on
+            )
+        }
+
+        WatchStatus.INTERRUPTED -> {
+            WatchListTvStatusUi(
+                statusLabel = "Interrupted • ${item.seasonInterruptedAt?.formatDate()}",
+                containerColor = MaterialTheme.colorScheme.errorContainer,
+                contentColor = MaterialTheme.colorScheme.onErrorContainer
+            )
+        }
+
+        else -> {
+            WatchListTvStatusUi(
+                statusLabel = "Added • ${item.seasonAddedDate.formatDate()}",
+                containerColor = statusColor.pending.bg,
+                contentColor = statusColor.pending.on
+            )
+        }
+    }
+
+
+}
