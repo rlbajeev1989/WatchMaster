c6a192aeef7dd16408610718a4b10d9787082d47
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-02 21:40:31 +0530

    Rename Genre to MovieGenre and update associated functions and constants

--- app/src/main/java/com/pranshulgg/watchmaster/data/MovieGenres.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/data/MovieGenres.kt
@@ -1,52 +1,52 @@
 package com.pranshulgg.watchmaster.data
 
-data class Genre(
+data class MovieGenre(
     val id: Int,
     val name: String
 )
 
-val LOCAL_GENRES = listOf(
-    Genre(28, "Action"),
-    Genre(12, "Adventure"),
-    Genre(10759, "Action & Adventure"),
+val LOCAL_MOVIE_GENRES = listOf(
+    MovieGenre(28, "Action"),
+    MovieGenre(12, "Adventure"),
+    MovieGenre(10759, "Action & Adventure"),
 
-    Genre(16, "Animation"),
-    Genre(35, "Comedy"),
-    Genre(80, "Crime"),
-    Genre(99, "Documentary"),
-    Genre(18, "Drama"),
+    MovieGenre(16, "Animation"),
+    MovieGenre(35, "Comedy"),
+    MovieGenre(80, "Crime"),
+    MovieGenre(99, "Documentary"),
+    MovieGenre(18, "Drama"),
 
-    Genre(10751, "Family"),
-    Genre(10762, "Kids"),
+    MovieGenre(10751, "Family"),
+    MovieGenre(10762, "Kids"),
 
-    Genre(14, "Fantasy"),
-    Genre(10765, "Sci-Fi & Fantasy"),
-    Genre(878, "Science Fiction"),
+    MovieGenre(14, "Fantasy"),
+    MovieGenre(10765, "Sci-Fi & Fantasy"),
+    MovieGenre(878, "Science Fiction"),
 
-    Genre(36, "History"),
-    Genre(27, "Horror"),
-    Genre(10402, "Music"),
+    MovieGenre(36, "History"),
+    MovieGenre(27, "Horror"),
+    MovieGenre(10402, "Music"),
 
-    Genre(9648, "Mystery"),
-    Genre(10749, "Romance"),
+    MovieGenre(9648, "Mystery"),
+    MovieGenre(10749, "Romance"),
 
-    Genre(53, "Thriller"),
-    Genre(10752, "War"),
-    Genre(10768, "War & Politics"),
+    MovieGenre(53, "Thriller"),
+    MovieGenre(10752, "War"),
+    MovieGenre(10768, "War & Politics"),
 
-    Genre(37, "Western"),
+    MovieGenre(37, "Western"),
 
-    Genre(10763, "News"),
-    Genre(10764, "Reality"),
-    Genre(10766, "Soap"),
-    Genre(10767, "Talk"),
+    MovieGenre(10763, "News"),
+    MovieGenre(10764, "Reality"),
+    MovieGenre(10766, "Soap"),
+    MovieGenre(10767, "Talk"),
 
-    Genre(10770, "TV Movie")
+    MovieGenre(10770, "TV Movie")
 )
 
 val GENRE_MAP: Map<Int, String> =
-    LOCAL_GENRES.associate { it.id to it.name }
+    LOCAL_MOVIE_GENRES.associate { it.id to it.name }
 
-fun getGenreNames(genreIds: List<Int>): List<String> {
+fun getMovieGenreNames(genreIds: List<Int>): List<String> {
     return genreIds.mapNotNull { GENRE_MAP[it] }
 }
