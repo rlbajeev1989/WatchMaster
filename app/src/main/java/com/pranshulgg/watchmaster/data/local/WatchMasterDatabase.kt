2ae53b9daf4ab50f2823fcd516d50f1a508d8e62
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-02 21:49:36 +0530

    Update database version to 23 and rename WatchlistSeasonEntity to SeasonEntity

--- app/src/main/java/com/pranshulgg/watchmaster/data/local/WatchMasterDatabase.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/data/local/WatchMasterDatabase.kt
@@ -8,8 +8,6 @@ import com.pranshulgg.watchmaster.data.local.dao.WatchlistDao
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
 import com.pranshulgg.watchmaster.data.local.converters.GenreIdsConverter
 import androidx.room.Room
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.pranshulgg.watchmaster.data.local.converters.InstantConverters
 import com.pranshulgg.watchmaster.data.local.converters.SeasonNameConverter
 import com.pranshulgg.watchmaster.data.local.converters.WatchStatusConverter
@@ -17,12 +15,12 @@ import com.pranshulgg.watchmaster.data.local.dao.MovieBundleDao
 import com.pranshulgg.watchmaster.data.local.dao.SeasonDao
 import com.pranshulgg.watchmaster.data.local.dao.TvBundleDao
 import com.pranshulgg.watchmaster.data.local.entity.MovieBundleEntity
-import com.pranshulgg.watchmaster.data.local.entity.WatchlistSeasonEntity
+import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
 import com.pranshulgg.watchmaster.data.local.entity.TvBundleEntity
 
 @Database(
-    entities = [WatchlistItemEntity::class, MovieBundleEntity::class, TvBundleEntity::class, WatchlistSeasonEntity::class],
-    version = 19
+    entities = [WatchlistItemEntity::class, MovieBundleEntity::class, TvBundleEntity::class, SeasonEntity::class],
+    version = 23
 )
 @TypeConverters(
     GenreIdsConverter::class,
@@ -49,7 +47,7 @@ abstract class WatchMasterDatabase : RoomDatabase() {
                     context.applicationContext,
                     WatchMasterDatabase::class.java,
                     "watchmaster.db"
-                ).addMigrations().build().also { INSTANCE = it }
+                ).fallbackToDestructiveMigration(true).build().also { INSTANCE = it }
             }
         }
     }
