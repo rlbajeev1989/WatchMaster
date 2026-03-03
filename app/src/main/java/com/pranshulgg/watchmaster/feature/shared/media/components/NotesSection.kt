320a148fad3e60356dcfa549b243f3074884d335
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-03 21:12:44 +0530

    Add NotesSection component

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/shared/media/components/NotesSection.kt
@@ -0,0 +1,55 @@
+package com.pranshulgg.watchmaster.feature.shared.media.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.pranshulgg.watchmaster.R
+import com.pranshulgg.watchmaster.core.ui.components.media.MediaSectionCard
+import com.pranshulgg.watchmaster.core.ui.theme.Radius
+
+@Composable
+fun NotesSection(isSeasonNoteNull: Boolean = true, action: () -> Unit, noteText: String) {
+    MediaSectionCard(
+        title = "Notes",
+        titleIcon = R.drawable.sticky_note_2_24px,
+        showAction = true,
+        actionOnClick = {
+            action()
+//            uiState = uiState.copy(showNoteDialog = true)
+        },
+        actionText = "Edit note"
+    ) {
+        if (!isSeasonNoteNull) {
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp)
+            ) {
+                Surface(
+                    color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                    shape = RoundedCornerShape(Radius.Medium),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(min = 64.dp)
+                ) {
+                    Text(
+                        noteText,
+                        modifier = Modifier.padding(10.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+
+        }
+    }
+    Spacer(modifier = Modifier.height(12.dp))
+}
