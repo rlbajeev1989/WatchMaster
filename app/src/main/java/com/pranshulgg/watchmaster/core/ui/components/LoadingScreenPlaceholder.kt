2546b0494d81b3675a13a635d2a9d1252cb16234
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-03 21:11:57 +0530

    Update LoadingScreenPlaceholder background and z-index

--- app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/LoadingScreenPlaceholder.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/core/ui/components/LoadingScreenPlaceholder.kt
@@ -1,21 +1,26 @@
 package com.pranshulgg.watchmaster.core.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun LoadingScreenPlaceholder(fraction: Float = 1f) {
     Box(
         modifier = Modifier
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+            .zIndex(10f)
             .fillMaxWidth()
             .fillMaxHeight(fraction = fraction),
         contentAlignment = Alignment.Center
