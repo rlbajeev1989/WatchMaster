cb7868ef9ddd89c2b88c90db53752e5f9bc32464
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-05 21:32:06 +0530

    Add UI state and dialog management to MovieDetailsViewModel

--- app/src/main/java/com/pranshulgg/watchmaster/feature/movie/detail/MovieDetailsViewModel.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/detail/MovieDetailsViewModel.kt
@@ -1,5 +1,6 @@
 package com.pranshulgg.watchmaster.feature.movie.detail
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -7,6 +8,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.pranshulgg.watchmaster.data.local.entity.MovieBundle
 import com.pranshulgg.watchmaster.data.repository.MovieRepository
+import com.pranshulgg.watchmaster.feature.tv.detail.TvDetailsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -31,4 +33,39 @@ class MovieDetailsViewModel @Inject constructor(
             loading = false
         }
     }
+
+    private val _uiState = mutableStateOf(MovieDetailsUiState())
+    val uiState: State<MovieDetailsUiState> = _uiState
+
+    fun showNoteDialog(note: String) {
+        _uiState.value = _uiState.value.copy(
+            showNoteDialog = true,
+            note = note
+        )
+    }
+
+    fun hideNoteDialog() {
+        _uiState.value = _uiState.value.copy(showNoteDialog = false)
+    }
+
+    fun updateNoteText(note: String) {
+        _uiState.value = _uiState.value.copy(note = note)
+    }
+
+    fun showRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = true)
+    }
+
+    fun hideRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = false)
+    }
+
+    fun showConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showConfirmationDialog = true)
+    }
+
+    fun hideConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showConfirmationDialog = false)
+    }
+
 }
