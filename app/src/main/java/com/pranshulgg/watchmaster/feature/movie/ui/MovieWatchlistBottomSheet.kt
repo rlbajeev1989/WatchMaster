3426c221de5d045113034b566bcc5fd1804ea2e9
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-07 19:51:31 +0530

    Add MovieWatchlistBottomSheet component

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/ui/MovieWatchlistBottomSheet.kt
@@ -0,0 +1,48 @@
+package com.pranshulgg.watchmaster.feature.movie.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.Composable
+import com.pranshulgg.watchmaster.core.ui.components.ActionBottomSheet
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.feature.movie.MovieHomeViewModel
+import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
+import com.pranshulgg.watchmaster.feature.shared.media.components.WatchlistMediaSheetContent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieWatchlistBottomSheet(
+    show: Boolean = false,
+    sheetState: SheetState,
+    watchlistItem: WatchlistItemEntity?,
+    onDismiss: () -> Unit,
+    viewModel: WatchlistViewModel,
+    movieHomeViewModel: MovieHomeViewModel
+) {
+
+    if (show && watchlistItem != null)
+        ActionBottomSheet(
+            showActions = false,
+            sheetState = sheetState,
+            onCancel = { onDismiss() },
+            onConfirm = { }
+        ) {
+            WatchlistMediaSheetContent(
+                id = watchlistItem.id,
+                mediaTitle = watchlistItem.title,
+                status = watchlistItem.status,
+                onUpdateRating = {
+                    movieHomeViewModel.showUpdateRatingDialog()
+                },
+                onStatusFinish = {
+                    movieHomeViewModel.showRatingDialog()
+                },
+                onDelete = {
+                    movieHomeViewModel.showConfirmationDialog()
+                },
+                onDismiss = { onDismiss() },
+                viewModel = viewModel,
+                isPinned = watchlistItem.isPinned,
+            )
+        }
+}
