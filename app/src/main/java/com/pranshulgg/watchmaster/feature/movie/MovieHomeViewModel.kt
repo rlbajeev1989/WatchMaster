51cf22db15b93eb898692c7a91584164e4d60408
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-07 19:51:23 +0530

    Add MovieHomeViewModel for movie feature management

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/MovieHomeViewModel.kt
@@ -0,0 +1,47 @@
+package com.pranshulgg.watchmaster.feature.movie
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.data.repository.WatchlistRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieHomeViewModel @Inject constructor(
+    private val watchlistRepository: WatchlistRepository
+) : ViewModel() {
+
+    private val _uiState = mutableStateOf(MovieHomeUiState())
+    val uiState: State<MovieHomeUiState> = _uiState
+
+
+    fun showRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = true)
+    }
+
+    fun showUpdateRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = true, isUpdateRating = true)
+    }
+
+    fun hideRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = false, isUpdateRating = false)
+    }
+
+    fun showBottomSheet(item: WatchlistItemEntity) {
+        _uiState.value = _uiState.value.copy(actionSheetItem = item, isSheetOpen = true)
+    }
+
+    fun hideBottomSheet() {
+        _uiState.value = _uiState.value.copy(isSheetOpen = false)
+    }
+
+    fun showConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showConfirmationDialog = true)
+    }
+
+    fun hideConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showConfirmationDialog = false)
+    }
+}
