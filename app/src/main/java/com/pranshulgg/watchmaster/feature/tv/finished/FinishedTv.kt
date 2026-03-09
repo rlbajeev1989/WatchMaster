a16bc6b13eee5c22344d2aa6cf7bae0127d695b8
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-09 20:14:14 +0530

    Implement list filtering and display logic in FinishedTv

--- app/src/main/java/com/pranshulgg/watchmaster/feature/tv/finished/FinishedTv.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/finished/FinishedTv.kt
@@ -1,11 +1,72 @@
 package com.pranshulgg.watchmaster.feature.tv.finished
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarScrollBehavior
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavController
+import com.pranshulgg.watchmaster.R
+import com.pranshulgg.watchmaster.core.model.WatchStatus
+import com.pranshulgg.watchmaster.core.ui.components.EmptyContainerPlaceholder
+import com.pranshulgg.watchmaster.core.ui.components.LoadingScreenPlaceholder
+import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
+import com.pranshulgg.watchmaster.feature.tv.components.TvItems
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun FinishedTv(
     isLoading: Boolean,
-    items: List<WatchlistItemEntity>
+    items: List<WatchlistItemEntity>,
+    scrollBehavior: FloatingToolbarScrollBehavior,
+    scrollBehaviorTopBar: TopAppBarScrollBehavior,
+    navController: NavController,
+    onLongActionTvRequest: (SeasonEntity, WatchlistItemEntity) -> Unit,
+    viewModel: WatchlistViewModel,
+    seasons: List<SeasonEntity>
 ) {
+
+    val seasonShowIds = remember(seasons) {
+        seasons.map { it.showId }.toSet()
+    }
+
+    val filteredItems = remember(items, seasonShowIds) {
+        items.filter { it.id in seasonShowIds }
+    }
+
+
+    if (isLoading) {
+        LoadingScreenPlaceholder()
+    }
+
+    if (filteredItems.isEmpty()) {
+        EmptyContainerPlaceholder(
+            R.drawable.movie_info_24px,
+            "No series found",
+            description = "Your watchlist is empty. Add some tv series to start building it!"
+        )
+    }
+
+
+    val (pinnedItems, normalItems) = remember(filteredItems) {
+        filteredItems.partition { it.isPinned }
+    }
+
+    TvItems(
+        scrollBehavior,
+        scrollBehaviorTopBar,
+        navController,
+        onLongActionTvRequest,
+        pinnedItems,
+        normalItems,
+        viewModel,
+        seasons
+    )
+
 }
