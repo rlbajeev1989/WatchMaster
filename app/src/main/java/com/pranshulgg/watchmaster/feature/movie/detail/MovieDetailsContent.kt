a6c34c5de14317c37dd2b8c716c54d3254ef7a85
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-09 20:14:51 +0530

    Add spacer at the end of MovieDetailsContent LazyColumn

--- app/src/main/java/com/pranshulgg/watchmaster/feature/movie/detail/MovieDetailsContent.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/detail/MovieDetailsContent.kt
@@ -1,5 +1,7 @@
 package com.pranshulgg.watchmaster.feature.movie.detail
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
@@ -7,6 +9,7 @@ import androidx.compose.material3.FloatingToolbarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.core.model.WatchStatus
 import com.pranshulgg.watchmaster.core.ui.components.media.MediaStatusSection
@@ -58,6 +61,7 @@ fun MovieDetailsContent(
                     watchlistItem.notes ?: ""
                 )
                 CastMovieSection(movieItem)
+                Spacer(modifier = Modifier.height(56.dp))
             }
         }
     }
