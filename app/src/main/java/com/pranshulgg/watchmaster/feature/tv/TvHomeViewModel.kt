808e823544ca17d36048e62b660dbd06de14593e
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-09 20:21:33 +0530

    Add TvHomeViewModel for managing TV home screen UI state

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/TvHomeViewModel.kt
@@ -0,0 +1,60 @@
+package com.pranshulgg.watchmaster.feature.tv
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.data.repository.WatchlistRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TvHomeViewModel @Inject constructor(
+    private val watchlistRepository: WatchlistRepository
+) : ViewModel() {
+
+    private val _uiState = mutableStateOf(TvHomeUiState())
+    val uiState: State<TvHomeUiState> = _uiState
+
+
+    fun showRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = true)
+    }
+
+    fun showUpdateRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = true, isUpdateRating = true)
+    }
+
+    fun hideRatingDialog() {
+        _uiState.value = _uiState.value.copy(showRatingDialog = false, isUpdateRating = false)
+    }
+
+    fun showBottomSheet(item: SeasonEntity, watchlistItem: WatchlistItemEntity) {
+        _uiState.value = _uiState.value.copy(
+            actionSheetWatchlistItem = watchlistItem,
+            actionSheetSeasonItem = item,
+            isSheetOpen = true
+        )
+    }
+
+    fun hideBottomSheet() {
+        _uiState.value = _uiState.value.copy(isSheetOpen = false)
+    }
+
+    fun showConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showConfirmationDialog = true)
+    }
+
+    fun showStatusConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showStatusConfirmationDialog = true)
+    }
+
+    fun hideStatusConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showStatusConfirmationDialog = false)
+    }
+
+    fun hideConfirmationDialog() {
+        _uiState.value = _uiState.value.copy(showConfirmationDialog = false)
+    }
+}
