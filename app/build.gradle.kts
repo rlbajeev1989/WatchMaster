50f387b367aa63772956ad7a3d0da73a7b272b4e
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-09 20:24:33 +0530

    Bump versionCode and versionName to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pranshulgg.watchmaster"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.0-beta5"
+        versionCode = 6
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
