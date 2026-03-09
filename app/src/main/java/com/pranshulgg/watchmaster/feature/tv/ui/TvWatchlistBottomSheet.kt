5ef383cee3854fc11837559c9f6ed1ab2977ad3e
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-09 20:21:55 +0530

    Update TvTabsContent to handle long action requests and pass season data

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/ui/TvWatchlistBottomSheet.kt
@@ -0,0 +1,58 @@
+package com.pranshulgg.watchmaster.feature.tv.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.Composable
+import com.pranshulgg.watchmaster.core.model.WatchStatus
+import com.pranshulgg.watchmaster.core.ui.components.ActionBottomSheet
+import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.feature.movie.MovieHomeViewModel
+import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
+import com.pranshulgg.watchmaster.feature.shared.media.components.WatchlistMediaSheetContent
+import com.pranshulgg.watchmaster.feature.tv.TvHomeViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TvWatchlistBottomSheet(
+    show: Boolean = false,
+    sheetState: SheetState,
+    seasonItem: SeasonEntity?,
+    watchlistItem: WatchlistItemEntity?,
+    onDismiss: () -> Unit,
+    viewModel: WatchlistViewModel,
+    tvHomeViewModel: TvHomeViewModel
+) {
+
+    if (show && seasonItem != null && watchlistItem != null)
+        ActionBottomSheet(
+            showActions = false,
+            sheetState = sheetState,
+            onCancel = { onDismiss() },
+            onConfirm = { },
+        ) {
+            WatchlistMediaSheetContent(
+                id = seasonItem.seasonId,
+                mediaTitle = "${watchlistItem.title}: ${seasonItem.name}",
+                status = seasonItem.status,
+                isTv = true,
+                onUpdateRating = {
+                    tvHomeViewModel.showUpdateRatingDialog()
+
+                },
+                onWatchStatus = { status ->
+                    if (status != WatchStatus.WATCHING) {
+                        tvHomeViewModel.showStatusConfirmationDialog()
+                    } else {
+                        tvHomeViewModel.showRatingDialog()
+                    }
+                },
+                onDelete = {
+                    tvHomeViewModel.showConfirmationDialog()
+                },
+                onDismiss = { onDismiss() },
+                viewModel = viewModel,
+                isPinned = watchlistItem.isPinned,
+            )
+        }
+}
