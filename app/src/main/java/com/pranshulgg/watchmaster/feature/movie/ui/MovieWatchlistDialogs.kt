e0e677d6f7c0284519c0fefe80391f1de66c2308
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-09 20:15:43 +0530

    Add MovieStatusWatchlistConfirmationDialog and refactor MovieWatchlistDialogs

--- app/src/main/java/com/pranshulgg/watchmaster/feature/movie/ui/MovieWatchlistDialogs.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/ui/MovieWatchlistDialogs.kt
@@ -2,19 +2,20 @@ package com.pranshulgg.watchmaster.feature.movie.ui
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.pranshulgg.watchmaster.core.model.WatchStatus
 import com.pranshulgg.watchmaster.core.ui.snackbar.SnackbarManager
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
 import com.pranshulgg.watchmaster.feature.movie.MovieHomeViewModel
 import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
 import com.pranshulgg.watchmaster.feature.shared.media.components.MediaConfirmationDialogContent
 import com.pranshulgg.watchmaster.feature.shared.media.components.MediaRatingDialogContent
+import com.pranshulgg.watchmaster.feature.shared.media.ui.watchstatus.confirmAction
 
 
 @Composable
 fun MovieWatchlistConfirmationDialog(
     watchlistViewModel: WatchlistViewModel,
     movieHomeViewModel: MovieHomeViewModel,
-    navController: NavController,
     watchlistItem: WatchlistItemEntity?,
 ) {
     val uiState = movieHomeViewModel.uiState.value
@@ -26,7 +27,6 @@ fun MovieWatchlistConfirmationDialog(
             onConfirm = {
                 watchlistViewModel.delete(item.id)
                 SnackbarManager.show("Movie deleted ${item.title}")
-                navController.popBackStack()
             }
         )
     }
@@ -49,9 +49,37 @@ fun MovieWatchlistRatingDialog(
             onConfirm = { rating ->
                 watchlistViewModel.setUserRating(item.id, rating)
                 watchlistViewModel.finish(item.id)
-
-                movieHomeViewModel.hideRatingDialog()
                 if (uiState.isUpdateRating) SnackbarManager.show("User rating updated")
+            }
+        )
+    }
+}
+
+
+@Composable
+fun MovieStatusWatchlistConfirmationDialog(
+    watchlistViewModel: WatchlistViewModel,
+    movieHomeViewModel: MovieHomeViewModel,
+    watchlistItem: WatchlistItemEntity?,
+) {
+    val uiState = movieHomeViewModel.uiState.value
+
+    watchlistItem?.let { item ->
+        MediaConfirmationDialogContent(
+            uiState.showStatusConfirmationDialog,
+            movieHomeViewModel::hideStatusConfirmationDialog,
+            status = item.status,
+            onConfirm = {
+                item.status.confirmAction(
+                    start = {
+                        watchlistViewModel.start(item.id)
+                    },
+                    finish = {
+                    },
+                    reset = {
+                        watchlistViewModel.reset(item.id)
+                    }
+                )
             }
         )
     }
