32e64dbed9b8f30d1c3a5aafadeba2daef59e187
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-11 21:38:28 +0530

    Add episode management and watch status functionality to TvRepository

--- app/src/main/java/com/pranshulgg/watchmaster/data/repository/TvRepository.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/data/repository/TvRepository.kt
@@ -6,13 +6,24 @@ import com.pranshulgg.watchmaster.data.local.entity.toDomain
 import com.pranshulgg.watchmaster.data.local.mapper.toDomain
 import com.pranshulgg.watchmaster.data.local.mapper.toEntity
 import com.pranshulgg.watchmaster.core.network.TmdbApi
+import com.pranshulgg.watchmaster.core.network.TvSeasonEpisodeDto
+import com.pranshulgg.watchmaster.core.network.TvSeasonEpisodesResponse
+import com.pranshulgg.watchmaster.data.local.dao.TvEpisodeDao
+import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
+import com.pranshulgg.watchmaster.data.local.entity.TvEpisodeEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+import java.time.Instant
 
 class TvRepository(
     private val api: TmdbApi,
-    private val dao: TvBundleDao
+    private val dao: TvBundleDao,
+    private val episodeDao: TvEpisodeDao
 ) {
 
-    suspend fun getWholeTvData(tvId: Long, seasonNumber: Int): TvBundle {
+    suspend fun getWholeTvData(tvId: Long): TvBundle {
 
         dao.getById(tvId)?.let {
             return it.toDomain()
@@ -24,10 +35,69 @@ class TvRepository(
         val domain = body.toDomain()
         dao.insert(domain.toEntity())
 
+
         return domain
     }
 
     suspend fun deleteCachedTv(id: Long) {
         dao.deleteById(id)
     }
+
+
+    suspend fun fetchEpisodes(
+        tvId: Long,
+        seasonId: Long,
+        seasonNumber: Int
+    ) {
+
+
+        val response = api.getTvSeasonEpisodes(tvId, seasonNumber)
+        val body = response.body() ?: error("TV episodes not found")
+
+        val domain = body.episodes
+
+
+        val eps = domain.map { episode ->
+            TvEpisodeEntity(
+                seasonId = seasonId,
+                showId = tvId,
+                air_date = episode.air_date,
+                episode_number = episode.episode_number,
+                name = episode.name,
+                overview = episode.overview,
+                still_path = episode.still_path,
+                runtime = episode.runtime,
+                isWatched = episode.isWatched,
+            )
+        }
+
+
+        episodeDao.insertEpisodes(eps)
+
+    }
+
+    suspend fun markEpWatched(epId: Long) {
+        episodeDao.updateEpisodeStatus(epId, true)
+    }
+
+    suspend fun markEpUnWatched(epId: Long) {
+        episodeDao.updateEpisodeStatus(epId, false)
+    }
+
+    fun getEpisodesForSeason(
+        tvId: Long,
+        seasonId: Long,
+        seasonNumber: Int
+    ): Flow<List<TvEpisodeEntity>> = flow {
+
+        val count = episodeDao.countEpisodes(seasonId)
+
+        if (count == 0) {
+            fetchEpisodes(tvId, seasonId, seasonNumber)
+        }
+
+        emitAll(episodeDao.getById(seasonId))
+    }
+
+
 }
