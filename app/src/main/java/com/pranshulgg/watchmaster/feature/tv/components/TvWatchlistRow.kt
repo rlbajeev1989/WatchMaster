ab94b388fb3ad2ad107de292e0016dcf39ce6f05
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-15 21:46:15 +0530

    Rename MaterialListShape to ListItemShape and clean up unused imports in TvWatchlistRow

--- app/src/main/java/com/pranshulgg/watchmaster/feature/tv/components/TvWatchlistRow.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/components/TvWatchlistRow.kt
@@ -1,40 +1,26 @@
 package com.pranshulgg.watchmaster.feature.tv.components
 
-import android.view.animation.RotateAnimation
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntSizeAsState
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -42,30 +28,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
 import com.pranshulgg.watchmaster.R
-import com.pranshulgg.watchmaster.core.model.SeasonData
-import com.pranshulgg.watchmaster.core.model.WatchStatus
-import com.pranshulgg.watchmaster.core.ui.components.MaterialListShape
+import com.pranshulgg.watchmaster.core.ui.components.ListItemShape
 import com.pranshulgg.watchmaster.core.ui.components.Symbol
 import com.pranshulgg.watchmaster.core.ui.components.media.PosterBox
-import com.pranshulgg.watchmaster.core.ui.components.media.PosterPlaceholder
-import com.pranshulgg.watchmaster.core.ui.navigation.NavRoutes
 import com.pranshulgg.watchmaster.core.ui.theme.Radius
 import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
-import com.pranshulgg.watchmaster.data.local.mapper.SeasonDataMapper
-import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
-import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -81,7 +55,7 @@ fun TvWatchlistRow(
     val isFirst = index == 0
     val isLast = index == items.lastIndex
 
-    val shape = MaterialListShape(isOnly, isFirst, isLast)
+    val shape = ListItemShape(isOnly, isFirst, isLast)
 
 
     var expanded by rememberSaveable(item.id) { mutableStateOf(false) }
@@ -174,7 +148,7 @@ fun TvWatchlistRow(
                         val isFirst = i == 0
                         val isLast = i == seasons.lastIndex
 
-                        val shapeSeasonRow = MaterialListShape(isOnly, isFirst, isLast)
+                        val shapeSeasonRow = ListItemShape(isOnly, isFirst, isLast)
 
                         SeasonTvRow(
                             season,
