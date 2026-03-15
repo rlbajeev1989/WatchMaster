e09d477c53a069d4b33adb53bf4cb051ce8a1e44
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-15 21:13:44 +0530

    Add EpisodeInfoSheetContent component

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/detail/components/EpisodeInfoSheetContent.kt
@@ -0,0 +1,44 @@
+package com.pranshulgg.watchmaster.feature.tv.detail.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pranshulgg.watchmaster.data.local.entity.TvEpisodeEntity
+
+@Composable
+fun EpisodeInfoSheetContent(
+    episode: TvEpisodeEntity,
+) {
+    EpisodeInfoHeader(episode)
+
+    Column(Modifier.padding(horizontal = 16.dp, vertical = 12.dp)) {
+
+        episode.overview?.let {
+
+            Text(
+                text = "Overview",
+                modifier = Modifier.padding(bottom = 5.dp),
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.W700
+            )
+            Text(
+                it,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+
+    }
+}
+
+
