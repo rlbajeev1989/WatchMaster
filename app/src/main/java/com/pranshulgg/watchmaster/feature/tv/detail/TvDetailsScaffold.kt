164b6b383daea3f1a9d9e9809659089c85484bc2
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-16 21:21:35 +0530

    Add WatchProviderSheet to TvDetailsScaffold

--- app/src/main/java/com/pranshulgg/watchmaster/feature/tv/detail/TvDetailsScaffold.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/detail/TvDetailsScaffold.kt
@@ -1,35 +1,29 @@
 package com.pranshulgg.watchmaster.feature.tv.detail
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingToolbarScrollBehavior
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.pranshulgg.watchmaster.core.model.WatchStatus
 import com.pranshulgg.watchmaster.core.ui.components.LoadingScreenPlaceholder
-import com.pranshulgg.watchmaster.core.ui.snackbar.SnackbarManager
 import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
-import com.pranshulgg.watchmaster.feature.shared.media.ui.MediaActionsFloatingToolbar
 import com.pranshulgg.watchmaster.feature.shared.media.ui.FloatingToolbarMediaActionsParams
+import com.pranshulgg.watchmaster.feature.shared.media.ui.MediaActionsFloatingToolbar
 import com.pranshulgg.watchmaster.feature.tv.detail.ui.TvDetailsConfirmationDialog
 import com.pranshulgg.watchmaster.feature.tv.detail.ui.TvDetailsNoteDialog
 import com.pranshulgg.watchmaster.feature.tv.detail.ui.TvDetailsRatingDialog
+import com.pranshulgg.watchmaster.feature.tv.detail.ui.WatchProviderSheet
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun TvDetailsScaffold(
@@ -49,6 +43,7 @@ fun TvDetailsScaffold(
     val watchlistItem by watchlistFlow.collectAsStateWithLifecycle()
     val episodesFlow = remember(seasonId) { viewModel.seasonEpisodes(seasonId) }
     val episodes by episodesFlow.collectAsStateWithLifecycle(initialValue = emptyList())
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
 
     val isSeriesPinned = watchlistItem?.isPinned == true
@@ -110,5 +105,10 @@ fun TvDetailsScaffold(
         seasons.size,
         navController
     )
-
+    WatchProviderSheet(
+        sheetState,
+        onDismiss = { viewModel.hideWatchProviderSheet() },
+        onConfirm = {},
+        viewModel
+    )
 }
