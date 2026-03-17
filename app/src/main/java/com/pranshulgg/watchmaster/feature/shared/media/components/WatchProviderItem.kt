93f5670dd2f8cdaa845fe10d5c6bb802ef686aec
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-17 21:03:04 +0530

    Add WatchProviderItem component

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/shared/media/components/WatchProviderItem.kt
@@ -0,0 +1,80 @@
+package com.pranshulgg.watchmaster.feature.shared.media.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pranshulgg.watchmaster.core.ui.components.media.PosterBox
+import com.pranshulgg.watchmaster.core.ui.theme.Radius
+import com.pranshulgg.watchmaster.data.Provider
+
+@Composable
+fun WatchProviderItem(title: String, providers: List<Provider>) {
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp),
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 5.dp),
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.W700
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            items(providers) {
+                Surface(
+                    shape = RoundedCornerShape(Radius.Medium),
+                    color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(
+                            top = 6.dp,
+                            bottom = 6.dp,
+                            start = 6.dp,
+                            end = 10.dp
+                        )
+                    ) {
+                        PosterBox(
+                            posterUrl = "https://image.tmdb.org/t/p/original${it.logo_path}",
+                            apiPath = it.logo_path,
+                            cornerRadius = Radius.Small,
+                            width = 30.dp,
+                            height = 30.dp
+                        )
+                        Spacer(Modifier.width(8.dp))
+                        Text(
+                            it.provider_name,
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
