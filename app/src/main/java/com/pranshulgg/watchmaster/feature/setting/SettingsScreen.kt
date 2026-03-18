6c781d2794c84f8a868e4344594d06a643afe6da
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-18 15:12:00 +0530

    Add default screen option and vertical spacing to SettingsScreen

--- app/src/main/java/com/pranshulgg/watchmaster/feature/setting/SettingsScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/setting/SettingsScreen.kt
@@ -1,6 +1,7 @@
 package com.pranshulgg.watchmaster.feature.setting
 
 import android.os.Build
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
 import com.pranshulgg.watchmaster.core.prefs.LocalAppPrefs
@@ -35,7 +37,8 @@ fun SettingsScreen(navController: NavController) {
                 Modifier
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
-                    .padding(paddingValues)
+                    .padding(paddingValues),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
             SettingSection(
                 title = "App looks",
@@ -84,9 +87,26 @@ fun SettingsScreen(navController: NavController) {
                             prefs.setDynamicColor(checked)
                         }
                     ),
+                ),
+            )
+            SettingSection(
+                title = "General",
+                tiles = listOf(
+                    SettingTile.DialogOptionTile(
+                        leading = { SettingsTileIcon(R.drawable.home_filled_24px) },
+                        title = "Starting default screen",
+                        options = listOf(
+                            "Home",
+                            "Movies",
+                            "TV series",
+                        ),
+                        selectedOption = prefs.defaultTab,
+                        onOptionSelected = {
+                            prefs.setDefaultTab(it)
+                        }
+                    )
                 )
             )
-
         }
     }
 }
