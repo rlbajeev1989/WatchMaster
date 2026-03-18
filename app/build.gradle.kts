8b1274c9b4b1d1c4d0ddf7a5ad12199bbfce3173
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-18 15:37:31 +0530

    Bump version to 1.3.0 and version code to 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pranshulgg.watchmaster"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.0"
+        versionCode = 10
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
