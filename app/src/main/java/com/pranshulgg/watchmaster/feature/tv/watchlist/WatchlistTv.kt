302b91dc08f4a26eabb991b33ddb99c05aee8774
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-19 19:55:28 +0530

    Clean up unused imports and whitespace in WatchlistTv

--- app/src/main/java/com/pranshulgg/watchmaster/feature/tv/watchlist/WatchlistTv.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/tv/watchlist/WatchlistTv.kt
@@ -1,28 +1,17 @@
 package com.pranshulgg.watchmaster.feature.tv.watchlist
 
-import androidx.compose.foundation.Image
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingToolbarScrollBehavior
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.navigation.NavController
 import com.pranshulgg.watchmaster.R
-import com.pranshulgg.watchmaster.core.model.WatchStatus
 import com.pranshulgg.watchmaster.core.ui.components.EmptyContainerPlaceholder
 import com.pranshulgg.watchmaster.core.ui.components.LoadingScreenPlaceholder
-import com.pranshulgg.watchmaster.core.ui.components.media.PosterBox
 import com.pranshulgg.watchmaster.data.local.entity.SeasonEntity
 import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
-import com.pranshulgg.watchmaster.data.local.mapper.SeasonDataMapper
-import com.pranshulgg.watchmaster.feature.movie.components.MovieItems
-import com.pranshulgg.watchmaster.feature.shared.WatchlistViewModel
 import com.pranshulgg.watchmaster.feature.tv.components.TvItems
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
@@ -66,7 +55,6 @@ fun WatchlistTv(
     val (pinnedItems, normalItems) = remember(filteredItems) {
         filteredItems.partition { it.isPinned }
     }
-
 
 
     TvItems(
