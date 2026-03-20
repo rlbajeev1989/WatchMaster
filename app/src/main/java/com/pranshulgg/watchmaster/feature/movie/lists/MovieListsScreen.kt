f7176e10733e3ad2cc83444decb9c457ae321a80
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-20 20:34:17 +0530

    Update MovieListsUiState to include listIcon and listMovieIds

--- app/src/main/java/com/pranshulgg/watchmaster/feature/movie/lists/MovieListsScreen.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/lists/MovieListsScreen.kt
@@ -1,13 +1,18 @@
 package com.pranshulgg.watchmaster.feature.movie.lists
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.pranshulgg.watchmaster.feature.movie.lists.create.ui.MovieListsSheet
 
 data class MovieListsUiState(
     val isSheetOpen: Boolean = false,
     val listName: String = "",
     val listDescription: String = "",
+    val listIcon: Int? = null,
+    val listMovieIds: List<Long> = emptyList()
 )
 
 @Composable
@@ -16,4 +21,5 @@ fun MovieListsScreen(navController: NavController) {
     val viewModel: MovieListsViewModel = hiltViewModel()
 
     MovieListsScaffold(navController, viewModel)
+
 }
