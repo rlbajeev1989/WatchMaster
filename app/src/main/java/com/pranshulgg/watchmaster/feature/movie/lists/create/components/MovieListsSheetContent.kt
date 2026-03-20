67abd564493067a373e60f4130e9a3de50c63a79
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-20 20:34:46 +0530

    Add MovieListsSheetContent component

---
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/lists/create/components/MovieListsSheetContent.kt
@@ -0,0 +1,17 @@
+package com.pranshulgg.watchmaster.feature.movie.lists.create.components
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+
+@Composable
+fun MovieListsSheetContent(items: List<WatchlistItemEntity>, onMovieSelect: (Long) -> Unit) {
+
+    LazyColumn() {
+        itemsIndexed(items, key = { _, item -> item.id }) { index, item ->
+            MovieListsSelectableItem(item, index, items, onMovieSelect)
+        }
+    }
+
+}
