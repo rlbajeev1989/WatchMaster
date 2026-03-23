54d9a1a93a641fac9fe5aaccb088258341586f14
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-03-23 21:57:24 +0530

    Update MovieListsContent to use MediaListsRow and display movie posters

--- app/src/main/java/com/pranshulgg/watchmaster/feature/movie/lists/MovieListsContent.kt
+++ app/src/main/java/com/pranshulgg/watchmaster/feature/movie/lists/MovieListsContent.kt
@@ -1,36 +1,89 @@
 package com.pranshulgg.watchmaster.feature.movie.lists
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ListItem
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.pranshulgg.watchmaster.R
-import com.pranshulgg.watchmaster.core.ui.components.TooltipIconBtn
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.pranshulgg.watchmaster.core.ui.components.AvatarMonogram
+import com.pranshulgg.watchmaster.core.ui.components.listItemShape
+import com.pranshulgg.watchmaster.core.ui.components.media.PosterBox
 import com.pranshulgg.watchmaster.data.local.entity.MovieListsEntity
+import com.pranshulgg.watchmaster.data.local.entity.WatchlistItemEntity
+import com.pranshulgg.watchmaster.feature.shared.media.components.MediaListsRow
 
 @Composable
-fun MovieListsContent(movieLists: List<MovieListsEntity>, onDelete: (Long) -> Unit) {
+fun MovieListsContent(
+    movieLists: List<MovieListsEntity>,
+    onLongPress: (Long) -> Unit,
+    movies: List<WatchlistItemEntity>
+) {
 
-    LazyColumn() {
-        items(
+    LazyColumn(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp)
+    ) {
+        itemsIndexed(
             movieLists,
-            key = { item -> item.id }) { item ->
+            key = { _, item -> item.id }) { index, item ->
 
-            ListItem(
-                headlineContent = { Text(item.name) },
-                supportingContent = {
-                    if (item.description.isNotEmpty()) {
-                        Text(item.description)
-                    } else null
+            val moviesFiltered = movies.filter { item.movieIds.contains(it.id) }
+
+            val isOnly = movieLists.singleOrNull() == item
+            val isFirst = index == 0
+            val isLast = index == movieLists.lastIndex
+
+            val shape = listItemShape(isOnly, isFirst, isLast)
+
+            MediaListsRow(
+                headline = item.name,
+                description = item.description,
+                shapes = shape,
+                onLongClick = {
+                    onLongPress(item.id)
                 },
-                trailingContent = { Text(item.movieIds.toString()) },
-                leadingContent = {
-                    TooltipIconBtn(
-                        onClick = { onDelete(item.id) },
-                        icon = R.drawable.delete_24px,
-                        tooltipText = "Delete"
-                    )
+                leading = {
+                    Box(modifier = Modifier.padding(top = 5.dp)) {
+                        AvatarMonogram(item.name.first().uppercase())
+                    }
+                },
+                media = {
+                    Row(
+                        modifier = Modifier.padding(top = 8.dp)
+                    ) {
+                        moviesFiltered.take(4).forEachIndexed { moveIndex, mov ->
+                            Box(
+                                modifier = Modifier.offset(x = (-12).dp * moveIndex)
+                            ) {
+                                PosterBox(
+                                    posterUrl = "https://image.tmdb.org/t/p/w154${mov.posterPath}",
+                                    apiPath = mov.posterPath,
+                                    width = 40.dp,
+                                    height = 40.dp,
+                                    circular = true
+                                )
+                            }
+                        }
+
+                        if (moviesFiltered.size > 4) {
+                            Box(
+                                modifier = Modifier.offset(x = (-12).dp * 4)
+                            ) {
+                                AvatarMonogram(
+                                    "${moviesFiltered.size}+",
+                                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                                    contentColor = MaterialTheme.colorScheme.onSurface
+                                )
+                            }
+                        }
+                    }
                 }
             )
         }
