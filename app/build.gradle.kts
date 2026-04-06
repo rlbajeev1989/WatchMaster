0496b826272be0cbef1732fe5d2322aaed0f741e
rlbajeev1989 <13780477+rlbajeev1989@users.noreply.github.com>
2026-04-06 21:39:14 +0530

    Bump version code to 13 and versionName to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pranshulgg.watchmaster"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.5.0"
+        versionCode = 13
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
